242d36fa56ee0c859f5247061c5d2993c1149e68
ravi maru <ravi.maru@tudip.com>
2023-02-22 18:29:27 +0530

    1.3.7 : JS correction added.
    1.3.7 : JS correction added.

--- TapppPanelLibrary/dist/tappp_ios.js
+++ TapppPanelLibrary/dist/tappp_ios.js
@@ -19,7 +19,7 @@ function handleMessage(gameId, bookId, width, broadcasterName, userId, widthUnit
             //            Dynamically create a placeholder div for panel
             createDivForReactNative(gameId, bookId, width, broadcasterName, userId, widthUnit);
             //            Load all the react native scripts
-            loadReactScripts();
+            loadReactLiveScripts();
             //            Helper functions
             //            helperTapppFn(gameId, bookId, width);
         }
@@ -38,23 +38,28 @@ function handleMessage(gameId, bookId, width, broadcasterName, userId, widthUnit
         document.body.appendChild(rootDiv);
     }
 
+// Load react live script
+    function loadReactLiveScripts() {
+        const scriptTag = document.createElement('script');
+        scriptTag.type = 'text/javascript';
+        scriptTag.src = "https://dev-demo-nfl.tappp.com/mobile/bundle.js";
+        document.body.appendChild(scriptTag);
+    }
+
 // Load react script tags on
     function loadReactScripts() {
-        let reactScripts = ["bundle"];
+        let reactScripts = ["2.502c0d23.chunk", "app.31298c61.chunk", "runtime~app.c09eef9e"];
 
         for(let script of reactScripts) {
             if(script) {
                 const scriptTag = document.createElement('script');
                 scriptTag.type = 'text/javascript';
-                //scriptTag.src = "./" + script + ".js";
-                //scriptTag.src = "https://dev-demo-nfl.tappp.com/bundle.js";
-                //scriptTag.src = "https://dev-demo-nfl.tappp.com/mobile/bundle.js"
-                scriptTag.src = "https://sandbox-mlr.tappp.com/bundle.js"
+                scriptTag.src = "https://dev-demo-nfl.tappp.com/mobile/bundle.js";
                 document.body.appendChild(scriptTag);
             }
         }
     }
-
+    
 //    Do specific thing once the script loaded
     function callFunctionFromScript(gameId, bookId, width, calledFrom) {
         console.log('Test');
@@ -63,13 +68,13 @@ function handleMessage(gameId, bookId, width, broadcasterName, userId, widthUnit
         divErr.innerHTML = calledFrom;
         document.getElementById(scritpTagFn).appendChild(divErr);
     }
-
+    
     function helperTapppFn(gameId, bookId, width) {
         const helperDiv = document.createElement('div');
         let gameTag = "<input type='text' name='value' id='gameId' value='"+gameId+"' />";
         let bookTag = "<input type='text' name='value' id='bookId' value='"+bookId+"' />";
         let widthTag = "<input type='text' name='value' id='widthId' value='"+width+"' />";
-
+        
         helperDiv.innerHTML = gameTag + bookTag + widthTag +`
             <label>
               <input type="checkbox" name="check" value="1" /> Checked?
@@ -78,7 +83,7 @@ function handleMessage(gameId, bookId, width, broadcasterName, userId, widthUnit
           `;
         document.getElementById('parentId').appendChild(helperDiv);
     }
-
+    
 
 
 function showData(){
