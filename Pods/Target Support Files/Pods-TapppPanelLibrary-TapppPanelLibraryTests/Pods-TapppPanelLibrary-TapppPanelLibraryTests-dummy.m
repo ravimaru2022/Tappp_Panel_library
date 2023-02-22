cc9a7470d7b66c678b94df2eea66cf96c55623ec
ravi maru <ravi.maru@tudip.com>
2023-02-22 12:52:08 +0530

    1.3.4 : Removed sentry for size optimisation.
    1.3.4 : Removed sentry for size optimisation.

--- Pods/Target Support Files/Pods-TapppPanelLibrary-TapppPanelLibraryTests/Pods-TapppPanelLibrary-TapppPanelLibraryTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TapppPanelLibrary_TapppPanelLibraryTests : NSObject
-@end
-@implementation PodsDummy_Pods_TapppPanelLibrary_TapppPanelLibraryTests
-@end
