0eb9b5e136ab3fff03ebacea058a394d34941c05
ravi maru <ravi.maru@tudip.com>
2023-03-01 17:18:42 +0530

    1.4.7 : JS correction added.
    1.4.7 : JS correction added.

--- TapppPanelLibrary/dist/tappp_ios.js
+++ TapppPanelLibrary/dist/tappp_ios.js
@@ -1,25 +1,18 @@
 
 function handleMessage(gameId, bookId, width, broadcasterName, userId, widthUnit) {
-    console.log('handleMessage details=', gameId, bookId, width, broadcasterName, userId, widthUnit);
+    console.log('handleMessage details hai=', gameId, bookId, width, broadcasterName, userId, widthUnit);
     try {
-        if(!broadcasterName) {
-            broadcasterName = 'NFL';
-        }
         if(!bookId) {
             bookId = '1000009';
-        }
-        if(!userId) {
-            userId = 'USR1234';
         }
         if(!widthUnit) {
             widthUnit = 'px';
         }
-
         if(gameId && bookId){
             //            Dynamically create a placeholder div for panel
             createDivForReactNative(gameId, bookId, width, broadcasterName, userId, widthUnit);
             //            Load all the react native scripts
-            loadReactScripts();
+            loadReactLiveScripts(broadcasterName);
             //            Helper functions
             //            helperTapppFn(gameId, bookId, width);
         }
@@ -28,33 +21,64 @@ function handleMessage(gameId, bookId, width, broadcasterName, userId, widthUnit
         document.getElementById('errorTag').innerHTML = "Oops something went wrong ...."+e;
     }
 }
-
+    
 //    Create root div for the Panel library
     function createDivForReactNative(gameId, bookId, width, broadcasterName, userId, widthUnit) {
         const rootDiv = document.createElement('div');
         const rootTag = "<div id='tappp-panel' userid='"+ userId +"' bookid='"+ bookId +"' gameid='"+ gameId +"' width='"+ width +"' widthunit='" + widthUnit +"' broadcastername='" + broadcasterName + "' ></div>";
         console.log('...rootTag=', rootTag);
         rootDiv.innerHTML = rootTag;
+
         document.body.appendChild(rootDiv);
+
     }
 
+// Load react live script
+    function loadReactLiveScripts(broadcasterName) {
+        const scriptTag = document.createElement('script');
+        scriptTag.type = 'text/javascript';
+        let tapppURL;
+        if(broadcasterName === 'NFL') {
+            tapppURL = 'https://dev-demo-nfl.tappp.com/';
+        } else if(broadcasterName === 'TRN') {
+            tapppURL = 'https://sandbox-mlr.tappp.com/';
+        }
+
+        console.log('tapppURL=', tapppURL);
+
+        scriptTag.src = tapppURL + "mobile/bundle.js";
+        document.body.appendChild(scriptTag);
+    }
+
+    function loadDynamicScriptData(panelData) {
+        let scriptTag = document.createElement('script');
+        var startTag = "(function () {";
+        var endTag = "})();";
+
+
+        var tapppPanelId = "var findId = document.getElementById('tappp-panel');"
+        var setPanelData = "var updateUserId = findId.setAttribute('panel-data', JSON.stringify(panelData));"
+        var finalTag = startTag + tapppPanelId + setPanelData + endTag;
+        console.log('loadDynamicScriptData=', finalTag);
+        scriptTag.innerHTML(finalTag);
+
+        document.body.appendChild(scriptTag);
+    }
+    
 // Load react script tags on
     function loadReactScripts() {
-        let reactScripts = ["bundle"];
-
+        let reactScripts = ["bundle.js"];
+        
         for(let script of reactScripts) {
             if(script) {
                 const scriptTag = document.createElement('script');
                 scriptTag.type = 'text/javascript';
-                //scriptTag.src = "./" + script + ".js";
-                //scriptTag.src = "https://dev-demo-nfl.tappp.com/bundle.js";
-                //scriptTag.src = "https://dev-demo-nfl.tappp.com/mobile/bundle.js"
-                scriptTag.src = "https://sandbox-mlr.tappp.com/bundle.js"
+                scriptTag.src = "./" + script + ".js";
                 document.body.appendChild(scriptTag);
             }
         }
     }
-
+    
 //    Do specific thing once the script loaded
     function callFunctionFromScript(gameId, bookId, width, calledFrom) {
         console.log('Test');
@@ -63,13 +87,13 @@ function handleMessage(gameId, bookId, width, broadcasterName, userId, widthUnit
         divErr.innerHTML = calledFrom;
         document.getElementById(scritpTagFn).appendChild(divErr);
     }
-
+    
     function helperTapppFn(gameId, bookId, width) {
         const helperDiv = document.createElement('div');
         let gameTag = "<input type='text' name='value' id='gameId' value='"+gameId+"' />";
         let bookTag = "<input type='text' name='value' id='bookId' value='"+bookId+"' />";
         let widthTag = "<input type='text' name='value' id='widthId' value='"+width+"' />";
-
+        
         helperDiv.innerHTML = gameTag + bookTag + widthTag +`
             <label>
               <input type="checkbox" name="check" value="1" /> Checked?
@@ -78,24 +102,9 @@ function handleMessage(gameId, bookId, width, broadcasterName, userId, widthUnit
           `;
         document.getElementById('parentId').appendChild(helperDiv);
     }
+    
 
 
-
-function showData(){
-//    console.log("showData called", gamedataObjectFromReferencePanel);
-//    document.getElementById('nameField').value = "xyz";
-}
-
-function fillDetailNew(){
-    //let nameField = document.getElementById('nameField').value;
-    //var email = document.getElementById("email").value;
-//    document.getElementById('welcome').innerHTML = "HTML static data";
-}
-
-/*function showPanelData(jsonString) {
-    //window.webkit.messageHandlers.postScript.postMessage(jsonString)
-    document.getElementById('welcome').innerHTML = jsonString;
-}*/
 function callNative() {
     webkit.messageHandlers.callNative.postMessage({
         "message": "callNative method data passes"
